07e835294445eacbd9488fbc4ba108fab0f2e7e7
Aogle2 <aaronogle7@gmail.com>
2024-07-15 23:35:52 -0700

    More testing with the GUI.

--- main/GuiTest.py
+++ main/GuiTest.py
@@ -11,11 +11,27 @@ root.title('Test')
 
 def change_text ():
     first_button.config(text="Change Text")
+    New_Window()
+
+#https://pythonguides.com/python-tkinter-multiple-windows-tutorial/
+HEIGHT = 300
+WIDTH = 500
+def New_Window():
+    Window = tk.Toplevel()
+    canvas = tk.Canvas(Window, height=HEIGHT, width=WIDTH)
+    a_button = Button(Window, text="Don't click here?", command=change_text)
+    a_button.place(x=150,y=25)
+    canvas.pack()
+    print("New Window!")
 
 
-first_button = Button(root, text = "Click Here", command=change_text)
+
+
+
+
+first_button = Button(root,text ="Click Here",command=change_text)
 first_button.place(x=150,y=25)
-
+print("Main Loop")
 root.mainloop()
-
+print("out of loop")
 #Look at this: https://www.geeksforgeeks.org/how-to-place-a-button-at-any-position-in-tkinter/
