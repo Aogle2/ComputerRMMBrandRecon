7c41bc7c60ef362931e1d5dfb10d09cc4e2da572
Aogle2 <aaronogle7@gmail.com>
2024-07-18 17:09:08 -0400

    Create the connection to the DB.

--- main/db_connect.py
+++ main/db_connect.py
@@ -6,9 +6,21 @@ def file_check(func):
     filepath= os.path.join(os.pardir,'Data',"TestDB.db")
     if Path(filepath):
         print("thisfile exist 1")
-        print(func())
 
 
 @file_check #Overview here: https://realpython.com/primer-on-python-decorators/
 def test():
     print("Test 2")
+
+
+
+#--------------------------------------------------
+
+def db_test():
+    DB = sql.connect(database= os.path.join(os.pardir,'Data',"TestDB.db"))
+    cur = DB.cursor()
+    cur.execute('SELECT sqlite_version()')
+    print(cur.fetchone()[-1])
+
+
+db_test()
