1796946b4ed6ca8f2856d7d28a4da31e380541f2
Aogle2 <aaronogle7@gmail.com>
2024-07-23 11:39:34 -0400

    Re-doing how I want this to work.

---
+++ main/dbfunctions.py
@@ -0,0 +1,7 @@
+"""
+
+use this file to create the functions needed to interact with the Database.
+This is going to use Pandas for the exporting of the DB into a csv or excel file.
+
+"""
+
---
+++ main/matplotfunctions.py
@@ -0,0 +1,5 @@
+"""
+This is going to have the fucntions that are using matplotlib.
+This is also going to use the functions for interacting with the DB.
+
+"""
