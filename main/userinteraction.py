30aa06d47ce178351f1a873c972fed77d3740b33
Aaron Ogle <aaronogle7@gmail.com>
2024-07-26 23:23:09 -0400

    What are design choices?!

--- main/userinteraction.py
+++ main/userinteraction.py
@@ -12,9 +12,20 @@ root.title("Computer Recon")
 
 
 #All teh buttons, and their placement in the grid as well as their commands.
-Button(root,text="About",command="").grid(row=0,column=0)
-Button(root, text="Summary",command="").grid(row=1,column=0)
-Button(root,text="Summary Visual",command="").grid(row=2,column=0)
+(Button(root,
+       text="About",
+       command="")
+        .grid(row=0,column=0))
+
+(Button(root,
+       text="Summary",
+       command="")
+        .grid(row=1,column=0))
+
+(Button(root,
+       text="Summary Visual",
+       command="")
+        .grid(row=2,column=0))
 
 
 #Control Variables for the CheckButton Widgets
@@ -28,8 +39,17 @@ def check_CSV_export():
 def check_Excel_export():
     print(excelControlVar.get())
 
-Checkbutton(root,text="CSV Export",command=check_CSV_export,variable=csvControlVar).grid(row=0,column=3)
-Checkbutton(root,text="Excel Export",command=check_Excel_export,variable=excelControlVar).grid(row=1,column=3)
+(Checkbutton(root,
+            text="CSV Export",
+            command=check_CSV_export,
+            variable=csvControlVar)
+                .grid(row=0,column=3))
+
+(Checkbutton(root,
+            text="Excel Export",
+            command=check_Excel_export,
+            variable=excelControlVar)
+                .grid(row=1,column=3))
 
 
 
