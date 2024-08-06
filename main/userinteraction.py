65db8ed36d4b19b3d4fcc7fec9f731afb3051f2c
Aaron Ogle <aaronogle7@gmail.com>
2024-08-06 08:50:00 -0400

    Start to work on the matplotlib part

--- main/userinteraction.py
+++ main/userinteraction.py
@@ -1,5 +1,6 @@
 import tkinter
 from pandastable import Table
+import matplotlib.pyplot as plt
 from tkinter import *
 import pandas
 import sqlite3
@@ -12,10 +13,31 @@ primary.resizable(width=False,height=False)
 primary.title("Computer Recon")
 
 
+
+
+def mpltest():
+    fig, ax = plt.subplots()
+
+    fruits = ['apple', 'blueberry', 'cherry', 'orange']
+    counts = [40, 100, 30, 55]
+    bar_labels = ['red', 'blue', '_red', 'orange']
+    bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+    ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+    ax.set_ylabel('fruit supply')
+    ax.set_title('Fruit supply by kind and color')
+    ax.legend(title='Fruit color')
+    plt.show()
+
+
 def aboutme():
      amWindow = tkinter.Toplevel()
      amWindow.geometry("200x200")
      tkinter.Label(amWindow,text=r"Made by AA-Ron Ogle. All this program does Is show a visual of data in a neat way.",wraplength=185,justify="center").pack()
+     mpltest()
+
+
 
 
 query = ("SELECT"
