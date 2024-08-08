5411f6b7d792f078ee486d9782ecb6c43ac4e00a
Aaron Ogle <aaronogle7@gmail.com>
2024-08-08 10:02:24 -0400

    Finsihed the plots and also clean up.

--- main/userinteraction.py
+++ main/userinteraction.py
@@ -1,5 +1,4 @@
 import tkinter
-from pandastable import Table
 import matplotlib.pyplot as plt
 from tkinter import *
 import pandas
@@ -12,10 +11,6 @@ primary.geometry('325x350')
 primary.resizable(width=False,height=False)
 primary.title("Computer Recon")
 
-def addlabels(x,y):
-    for i in range(len(x)):
-        plt.text(i,y[i],y[i])
-
 #Consolidate how I want the data to be retreaved, saves time instead of typing this over and over.
 def basequery(query):
     cnx = sqlite3.connect(database=os.path.join(os.pardir, 'data', "TestDB.db"))
@@ -27,40 +22,46 @@ def basequery(query):
 #https://www.geeksforgeeks.org/adding-value-labels-on-a-matplotlib-bar-chart/
 
 def venderview():
-    query = basequery("SELECT vtype as [Vendor Type], COUNT(vName) as Count FROM Vendor GROUP BY vtype;")
+    query = basequery("SELECT vtype as [Vendor Type], COUNT(vName) as Count FROM Vendor GROUP BY vtype ORDER BY COUNT(vName);")
     query['Vendor Type'] = query['Vendor Type'].astype(str)
-    #Split the df into two list
-
-    plt.bar(query['Vendor Type'], query['Count'])
+    fig, ax = plt.subplots()
+    bars = ax.bar(query['Vendor Type'], query['Count'])
+    for bar in bars:
+        height = bar.get_height()
+        ax.annotate(f'{height}',xy=(bar.get_x() + bar.get_width() /2,height), xytext=(0,2),textcoords='offset points',ha='center',va='bottom')
     plt.xlabel('Vendor Type')
     plt.ylabel('Count')
-    plt.title('Count of Vendors Used')  # Rotate x-axis labels if needed
-    plt.tight_layout()  # Adjust layout to prevent clipping of labels
+    plt.title('What are the most common vendors are present? ')
+    plt.tight_layout()
     plt.show()
 
 
 def osview():
-    query = basequery("SELECT [OS Manufacturer], COUNT([OS Name]) AS Count FROM OperatingSystem GROUP BY [OS Manufacturer];")
+    query = basequery("SELECT [OS Manufacturer], COUNT([OS Name]) AS Count FROM OperatingSystem GROUP BY [OS Manufacturer] ORDER BY COUNT([OS Name]);")
     query.fillna('Linux', inplace=True)
     query['OS Manufacturer'] = query['OS Manufacturer'].astype(str)
-    #Split the query into two list
-
-    plt.bar(query['OS Manufacturer'], query['Count'])
+    fig,ax = plt.subplots()
+    bars = ax.bar(query['OS Manufacturer'], query['Count'])
+    for bar in bars:
+        height = bar.get_height()
+        ax.annotate(f'{height}',xy=(bar.get_x() + bar.get_width() /2,height), xytext=(0,2),textcoords='offset points',ha='center',va='bottom')
     plt.xlabel('Operating System')
     plt.ylabel('Count')
-    plt.title('Count of Operating Systems')  # Rotate x-axis labels if needed
+    plt.title('Count of Operating Systems Used in Enviroment')  # Rotate x-axis labels if needed
     plt.tight_layout()  # Adjust layout to prevent clipping of labels
     plt.show()
 
 def manufacturersummary():
     query = basequery("SELECT vName as [Vendor Name], COUNT(vtype) as Count FROM Model JOIN Vendor ON Model.v_id = Vendor.v_id GROUP BY vType ORDER BY (COUNT(vName)) LIMIT 5")
     query['Vendor Name'] = query['Vendor Name'].astype(str)
-    #Split the query into two list.
-
-    plt.bar(query['Vendor Name'], query['Count'])
+    fig, ax = plt.subplots()
+    bars = ax.bar(query['Vendor Name'], query['Count'])
+    for bar in bars:
+        height = bar.get_height()
+        ax.annotate(f'{height}',xy=(bar.get_x() + bar.get_width() /2,height), xytext=(0,2),textcoords='offset points',ha='center',va='bottom')
     plt.xlabel('Vendor Name')
     plt.ylabel('Count')
-    plt.title('Count of Vendors Used')  # Rotate x-axis labels if needed
+    plt.title('How many devices are using what Manufacturer?')  # Rotate x-axis labels if needed
     plt.tight_layout()  # Adjust layout to prevent clipping of labels
     plt.show()
 
